bcfb4cb32614e2af869b54259f4065c40457bd18
NisriinaWakhdah <ninaputri3344@gmail.com>
2025-09-04 11:21:46 +0700

    Deployment PWS

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -32,7 +32,7 @@ PRODUCTION = os.getenv('Production', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nisriina-wakhdah-footballshop.pbp.cs.ui.ac.id"]
 
 
 # Application definition
