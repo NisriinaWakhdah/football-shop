d7dc5a218ac79f5ef68daaf9a7cb76c12ae6debf
NisriinaWakhdah <ninaputri3344@gmail.com>
2025-09-20 22:52:08 +0700

    Tugas Individu 4

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nisriina-wakhdah-footballshop.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://nisriina-wakhdah-footballshop.pbp.cs.ui.ac.id/",
+    "https://nisriina-wakhdah-footballshop.pbp.cs.ui.ac.id",
 ]
 
 # Application definition
